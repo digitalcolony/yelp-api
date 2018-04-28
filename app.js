5c92eb93140e2aff998629b97bf09383dbb9b64e
MAS <digitalcolony@gmail.com>
2018-04-28 14:27:05 -0700

    examples now from array forEach

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const search_business_by_id = async yelp_id => {
 };
 
 // Using Woodlands Coffee as an Active Business Example
-search_business_by_id("6AFJ3a2-iCaRd04R8fXuCw").then(business => {
-  console.log(`Name: ${business.name}  Closed: ${business.is_closed}`);
-});
-
 // Using Neptune Coffee as a Closed Business Example
-search_business_by_id("_hOl9ZqJeT6CvkDN9NaQKw").then(business => {
-  console.log(`Name: ${business.name}  Closed: ${business.is_closed}`);
+const yelp_ids = ["6AFJ3a2-iCaRd04R8fXuCw", "_hOl9ZqJeT6CvkDN9NaQKw"];
+
+yelp_ids.forEach(id => {
+  search_business_by_id(id).then(business => {
+    console.log(`Name: ${business.name}  Closed: ${business.is_closed}`);
+  });
 });

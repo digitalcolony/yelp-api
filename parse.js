18a1ed26b257ccb0d707ad41fd613292b9dc6b79
MAS <digitalcolony@gmail.com>
2018-04-30 18:53:51 -0700

    Added search_business_by_match

--- parse.js
+++ parse.js
@@ -1,11 +1,11 @@
 // parse yelp.json file (for offline testing)
 const fs = require("fs");
 
-fs.readFile("yelp.json", (err, data) => {
+fs.readFile("match.json", (err, data) => {
   if (err) {
     console.log("Error trying to open file.");
     return;
   }
   obj = JSON.parse(data);
-  console.log(obj.alias);
+  console.log(obj.businesses[0].id);
 });

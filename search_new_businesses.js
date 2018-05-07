14b6f5c9aa26f47c04a3e18520b5c9e615a86a68
MAS <digitalcolony@gmail.com>
2018-05-07 15:19:04 -0700

    search new places

---
+++ search_new_businesses.js
@@ -0,0 +1,29 @@
+const yelp = require("./lib/yelp-api");
+
+const search = {
+  categories: `coffee`,
+  location: `98407`
+};
+
+yelp
+  .search_new_businesses(search.categories, search.location)
+  .then(businesses => {
+    const num_found = Object.keys(businesses.businesses).length;
+    if (num_found === 0) {
+      console.log(`Search returned 0 results`);
+    } else {
+      for (let index of businesses.businesses.keys()) {
+        process.stdout.write(
+          `[${index + 1}] ${businesses.businesses[index].name}`
+        );
+        console.log(
+          ` ${businesses.businesses[index].location.address1}, ${
+            businesses.businesses[index].location.city
+          }`
+        );
+        let yelp_url = businesses.businesses[index].url;
+        let yelp_url_array = yelp_url.split("?");
+        console.log(` ${yelp_url_array[0]}`);
+      }
+    }
+  });

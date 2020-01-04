e75f8d22fc1992cee6e6a685cc5f75d7127f04cc
Michael Allen Smith <digitalcolony@gmail.com>
2020-01-04 11:08:03 -0800

    updated NPM packages

--- search_new_businesses.js
+++ search_new_businesses.js
@@ -1,8 +1,9 @@
 const yelp = require("./lib/yelp-api");
+const fs = require("fs");
 
 const search = {
   categories: `coffee`,
-  location: `98407`
+  location: `98003`
 };
 
 yelp
@@ -12,6 +13,7 @@ yelp
     if (num_found === 0) {
       console.log(`Search returned 0 results`);
     } else {
+      //fs.writeFile("newcafes.json", JSON.stringify(businesses));
       for (let index of businesses.businesses.keys()) {
         process.stdout.write(
           `[${index + 1}] ${businesses.businesses[index].name}`
